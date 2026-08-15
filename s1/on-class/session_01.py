e2dbc1fef659f8fc2ef5285b22d3736976ee85c1
Prograin <ahmadreza.pcg1377@gmail.com>
2026-08-15 04:48:46 -0700

    task: new tasks are added

---
+++ s1/on-class/session_01.py
@@ -0,0 +1,33 @@
+# Variable
+x = 10
+alisname = 'alireza'
+
+# 01ali = 12
+ali10 = 12
+
+new_folder = 20
+
+
+print(ali10-10)
+
+
+# Datatype
+age = 20  # Type is : Integer
+weight = 22.5  # Type is : float
+
+my_name = 'Ahmad'  # Type is String
+
+is_active = True  # Type is Boolean
+is_active = False  # Type is Boolean
+
+list_1 = [20, 67, True, 'ali']  # Type is list
+
+print(list_1)
+
+student_1 = 'reza'
+student_2 = 'mohammad'
+student_3 = 'javad'
+
+my_students_list = [student_1, student_2, student_3]
+
+print(student_1, student_2, student_3)
