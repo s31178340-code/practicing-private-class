cc5db3ae0022f57247423ff1fb7359336a05f5db
Prograin <ahmadreza.pcg1377@gmail.com>
2026-08-20 12:18:20 -0700

    review: tasks are reviewd

--- s1/mohammadali_answers/first task.py
+++ s1/mohammadali_answers/first task.py
@@ -1,11 +1,13 @@
+# باید کامنت بشن
 1
-hamid= 30
-health= "full"
-powerful="yes"
-professional= "no"
+hamid = 30
+health = "full"
+powerful = "yes"
+professional = "no"
 
+# باید کامنت بشن
 2
-playersname= 'kain'
-salary= '60mil'
-health= "perfect"
-is_alive= True
+playersname = 'kain'
+salary = '60mil'
+health = "perfect"
+is_alive = True
--- s1/mohammadali_answers/on-class/01.py
+++
@@ -1,9 +0,0 @@
-age=19
-age=19
-my_name="mohamad"
-role_12=20
-my_address= "tehran valiasr 05"
-age_1= 22.4
-weight= 12
-list_=["reza", "mohammad"]
-print(age_1)
