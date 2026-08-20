ddbe149c1dd41e073e1d317ebabc16f9efd98f10
mohammad <nexo@examples.com>
2026-08-20 23:25:01 +0330

    recive

---
+++ s1/mohammadali_answers/second task.py
@@ -0,0 +1,21 @@
+1
+players_coin= 5000
+price_of_sh= 1800
+price_of_ze= 2200
+totaly= 1800+2200
+result= 5000-4000
+print('مقدار سکه باقی مانده:',result)
+2
+random_team=1200
+print('each player has', 1200//4, 'coins')
+3
+Bullet=23
+print(23%5, 'bullets left')
+4
+player_health=80
+enemy_health =65
+print(player_health>enemy_health)
+5
+a=2000
+b=2100
+print(a>b)
