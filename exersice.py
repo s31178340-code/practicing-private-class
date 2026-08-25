3b5a238029956ddc237747c86297dd463e0c5b83
mohammad <nexo@examples.com>
2026-08-25 13:43:46 +0330

    new but no

---
+++ exersice.py
@@ -0,0 +1,6 @@
+a = 5
+b=1
+if a<b :
+    print(2)
+elif b<a:
+    print(6)
---
+++ s1/mohammadali_answers/third_task.py
@@ -0,0 +1,16 @@
+#1
+username= input('enter your wallet balance:')
+a= username
+b= int(username)
+print(int(username))
+
+amount= input("enter your card balance:")
+if amount>100 :
+    print('you can buy the car')
+elif amount>400 :
+    print("you can buy the house")
+else:
+    print('you can buy nothing')
+
+
+
