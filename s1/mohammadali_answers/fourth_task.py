0a4ebb8891cecf8311b5aeb9d9144a04b1c29096
Prograin <ahmadreza.pcg1377@gmail.com>
2026-08-28 06:45:58 -0700

    task:review

--- s1/mohammadali_answers/fourth_task.py
+++ s1/mohammadali_answers/fourth_task.py
@@ -1,27 +1,35 @@
-#1
-users_password= 1234
-users_username= 'iran'
-user= input("enter your password:")
-user= int(user)
-if users_password==user:
+# 1
+users_password = 1234
+users_username = 'iran'
+user = input("enter your password:")
+user = int(user)
+
+# اینجا باید از and استفاده کنی
+# if users_password == user and username_from_input == user_name:
+if users_password == user:
     print("you are entered.")
-else :
+else:
     print('you are not entered')
-user_1=input('enter your username:')
-if user_1== 'iran':
-    print('you are entered now')#
+
+user_1 = input('enter your username:')
+if user_1 == 'iran':
+    print('you are entered now')
 # -------------------------------------------------
-#2
-user= input("enter your age:")
-user=int(user)
-if user>= 19 :
+# 2
+user = input("enter your age:")
+user = int(user)
+
+# مجدد از and
+# has_card = True
+# if user >= 19 and has_card == True:
+
+if user >= 19:
     print('you are allowed')
-else :
+else:
     print('you are not allowed')
-user= input("do you have card right now?")
-if user== True:
+
+user = input("do you have card right now?")
+if user == True:
     print('you are able to enter')
 else:
     print('you are not able to enter')
-
-
