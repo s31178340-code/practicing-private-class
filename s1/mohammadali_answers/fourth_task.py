6d447163f7ee92b0398edb55c85982cadaae311d
mohammad <nexo@examples.com>
2026-08-28 20:39:29 +0330

    new

--- s1/mohammadali_answers/fourth_task.py
+++ s1/mohammadali_answers/fourth_task.py
@@ -1,35 +1,50 @@
-# 1
-users_password = 1234
-users_username = 'iran'
-user = input("enter your password:")
-user = int(user)
+# # 1
+# users_password = 1234
+# users_username = 'iran'
+# user = input("enter your password:")
+# user = int(user)
 
-# اینجا باید از and استفاده کنی
-# if users_password == user and username_from_input == user_name:
-if users_password == user:
-    print("you are entered.")
-else:
-    print('you are not entered')
+# # اینجا باید از and استفاده کنی
+# # if users_password == user and username_from_input == user_name:
+# if users_password == user:
+#     print("you are entered.")
+# else:
+#     print('you are not entered')
 
-user_1 = input('enter your username:')
-if user_1 == 'iran':
-    print('you are entered now')
-# -------------------------------------------------
-# 2
-user = input("enter your age:")
-user = int(user)
+# user_1 = input('enter your username:')
+# if user_1 == 'iran':
+#     print('you are entered now')
+# # -------------------------------------------------
+# # 2
+# user = input("enter your age:")
+# user = int(user)
 
-# مجدد از and
-# has_card = True
-# if user >= 19 and has_card == True:
+# # مجدد از and
+# # has_card = True
+# # if user >= 19 and has_card == True:
 
-if user >= 19:
-    print('you are allowed')
-else:
-    print('you are not allowed')
+# if user >= 19:
+#     print('you are allowed')
+# else:
+#     print('you are not allowed')
 
-user = input("do you have card right now?")
-if user == True:
-    print('you are able to enter')
-else:
-    print('you are not able to enter')
+# user = input("do you have card right now?")
+# if user == True:
+#     print('you are able to enter')
+# else:
+#     print('you are not able to enter')
+
+# #3
+# my_list=[]
+# user= input('enter a word:')
+# my_list.append(user)
+# print(my_list)
+
+#4
+list=['milk', 'icecream', '5_toys']
+a= 'notella'
+list.append(a)
+extra_product=list[0]
+print(type(extra_product))
+
+
