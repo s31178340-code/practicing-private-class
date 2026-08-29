9fd577d8bbeb9cdceeec67690263d8be9c81e4c7
Prograin <ahmadreza.pcg1377@gmail.com>
2026-08-29 13:58:14 -0700

    task: new task is added

---
+++ s1/on-class/session_06.py
@@ -0,0 +1,14 @@
+# Logical Operations
+
+# a = 6
+# b = 10
+
+# if a > 10 or b > 5:
+#     print("Both of them are true")
+# else:
+#     print("that condition was not runned")
+
+
+for fff in [10, 123, 12, 3, 123, 12, 'asd', 'asds']:
+    print(fff)
+    print('------------')
