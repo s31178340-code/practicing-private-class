f64d48c80ae841ef204a63994b51f8c5162ef6cc
mohammad <nexo@examples.com>
2026-08-30 17:13:35 +0330

    new

--- exersice.py
+++ exersice.py
@@ -1,6 +1,22 @@
-a = 5
-b=1
-if a<b :
-    print(2)
-elif b<a:
-    print(6)
+# correct_password= '1234'
+# correct_username= 'ali'
+# user= input('enter your password:')
+# user_1= input("enter your username:")
+# if user==correct_password and user_1==correct_username:
+#     print('you are allowed to enter here:')
+# else :
+#     print('you are not allowed to enter here:')
+
+# has_car= 'True'
+# Target_city= 'arak'
+# user= input('enter your target city:')
+# user_1= input('do you have a personal vehicle:')
+# if user==Target_city and user_1==has_car :
+#     print('you can travel')
+# else :
+#     print('no')
+
+# for a in[12 , 250, 1233] :
+#     if a>12 :
+#         print('its greater', a)
+
