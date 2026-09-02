8284b19053df735d3052b2df53166d020d735c0e
Prograin <ahmadreza.pcg1377@gmail.com>
2026-09-02 10:00:03 -0700

    task: new tasks are added

---
+++ s1/code/07_function.py
@@ -0,0 +1,23 @@
+def tamrin_1():
+    print("hello")
+
+
+def multiply_by_two(a):
+    print(a*2)
+
+
+# multiply_by_two(4)
+# multiply_by_two(5)
+# multiply_by_two(20)
+
+
+def multiply(numb_1, numb_2):
+    n = numb_1*numb_2
+    return n
+
+
+m = multiply(4, 7)
+p = multiply(10, 3)
+
+print(m)
+print(p)
--- s1/mohammadali_answers/fifth_task.py
+++ s1/mohammadali_answers/fifth_task.py
@@ -1,31 +1,43 @@
-##1
-# a=[1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20]
-# n= a[:4]
-# for b in n:
-#     print(b)
+# 1
+def practice_1():
+    a = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20]
+    n = a[:4]
+    for b in n:
+        print(b)
 
-#2
-# a=[]
-# user= input('enter a vlue:')
-# b=int(user)
-# a.append(b)
-# print(a)
 
-##3
-# list=['milk', 'icecream', '5_toys']
-# a= 'notella'
-# list.append(a)
-# extra_product=list[0]
-# print(type(extra_product))
+# 2
+def practice_2():
+    a = []
+    user = input('enter a vlue:')
+    b = int(user)
+    a.append(b)
+    print(a)
 
-#4
-user= input('enter your score:')
-user= int(user)
-if user>=18 and user<=20 :
-    print("Excellent")
-elif user<18 and user>=15 :
-    print('nice')
-elif user<15 and user>=10 :
-    print('acceptable')
-elif user<10 :
-    print('rejected')
+# 3
+
+
+def practice_3():
+    list = ['milk', 'icecream', '5_toys']
+    a = 'notella'
+    list.append(a)
+    extra_product = list[0]
+    print(type(extra_product))
+
+# 4
+
+
+def practice_4():
+    user = input('enter your score:')
+    user = int(user)
+    if user >= 18 and user <= 20:
+        print("Excellent")
+    elif user < 18 and user >= 15:
+        print('nice')
+    elif user < 15 and user >= 10:
+        print('acceptable')
+    elif user < 10:
+        print('rejected')
+
+
+practice_2()
