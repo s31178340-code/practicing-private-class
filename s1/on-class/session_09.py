1c7095437b19ef49c54e30c918ab6f3788f47c71
Prograin <ahmadreza.pcg1377@gmail.com>
2026-09-05 14:52:32 -0700

    tasks: new tasks are added

---
+++ s1/on-class/session_09.py
@@ -0,0 +1,16 @@
+# import math
+# import random
+
+from math import sqrt, pi, sin
+# from random import randint
+
+
+# import math as m
+
+# print(math.pi)
+# print(math.sqrt(9))
+# print(m.sqrt(9))
+# print(sqrt(9))
+# print(randint(60, 1000))
+# print(random.randint(60, 1000))
+# print(random.choice(["ali", "javad", "morteza", True, 12]))
