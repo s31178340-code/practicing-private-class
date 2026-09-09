21d7be759de8aa3194c0f6b72760a37a69e87f2b
mohammad <nexo@examples.com>
2026-09-09 14:56:13 +0330

    new

---
+++ s1/mohammadali_answers/seventh_task.py
@@ -0,0 +1,21 @@
+# #1
+# import math 
+# print(math.sqrt(64))
+
+# #2
+# from math import sqrt
+# print(sqrt(81))
+
+# #3
+# import math
+# print(math.sin(12), math.cos(12))
+
+#4
+# import math
+# def user():
+#     user=input("enter  a value:")
+#     a=print(math.sqrt(user))
+#     print(a**2)
+
+# user()   به نتیجه نرسید
+
